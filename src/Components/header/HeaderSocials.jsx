3d83f11178518175dd188be1610cf2ae2abccad8
Keiroa <keiroa410@gmail.com>
2022-12-27 01:32:48 -0800

    Added React icons and modified header text

--- src/Components/header/HeaderSocials.jsx
+++ src/Components/header/HeaderSocials.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/kevin-rodass/" target="_blank">LinkedIn</a>
-        <a href="https://github.com/Keiroa" target="_blank">Github</a>
+        <a href="https://www.linkedin.com/in/kevin-rodass/" target="_blank"><BsLinkedin /></a>
+        <a href="https://github.com/Keiroa" target="_blank"><FaGithub /></a>
     </div>
   )
 }
